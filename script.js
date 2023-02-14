d95c73ceebf5b8575df7520b3d0fcd2872f8de05
Kelpering <tristan8272@gmail.com>
2023-02-14 09:06:14 -0700

    submit

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
 
-    $("p").click(function()
+    $("#prehtml").click(function()
     {
-        $(this).hide();
+        $(this)[0].checked //add prehtml code
+    })
+    $("#bootstrap").click(function()
+    {
+        $(this)[0].val() //add bootstrap ver code
+    })
+    $("#jquery").click(function()
+    {
+        $(this)[0].checked //add jquery code
     })
 
 })
+
+
+
+var part2 = function()
+{
+    //take all answers, generate prewritten code for boxes
+    $("#part1").hide();
+    $("#part2").show();
+}
+
+var prewritten = "&lt;!Doctype html&gt;\n&lt;html&gt;\n&lt;head&gt;\n\n&lt;/head&gt;\n&lt;body&gt;\n\n&lt;/body&gt;\n&lt;/html&gt;"
